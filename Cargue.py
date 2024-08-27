64d0265773e7168429e207c115b7922c01a81f7d
hector fabio <hector.londono@utp.edu.co>
2024-08-27 17:25:07 -0500

    pruebas dos de cargue

--- Cargue.py
+++ Cargue.py
@@ -9,11 +9,17 @@ Original = pd.read_excel(file_path2, engine= 'openpyxl')
 # Asegurarse de que la columna de ID tenga el mismo nombre en ambos DataFrames
 id_columna = 'PER_ID'
 
-# Buscar registros en df_comparar que no estén en df_base
+# Buscar registros en Nuevo que no estén en Original
 nuevos_registros = Nuevo[~Nuevo[id_columna].isin(Original[id_columna])]
+
+# Agregar una columna de marcador en los nuevos registros
+nuevos_registros['Es_Nuevo'] = True
 
 # Insertar nuevos registros en Original
 df_actualizado = pd.concat([Original, nuevos_registros], ignore_index=True)
+
+# Para los registros que ya estaban en Original, asegurarse de que la columna de marcador esté en False
+df_actualizado['Es_Nuevo'].fillna(False, inplace=True)
 
 # Guardar el DataFrame actualizado en un nuevo archivo Excel o sobreescribir el existente
 df_actualizado.to_excel('archivo_base_actualizado.xlsx', index=False)
