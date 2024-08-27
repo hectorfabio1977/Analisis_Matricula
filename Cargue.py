4d39d732df6a840b4b9b5249864051d59c64ce98
hector fabio <hector.londono@utp.edu.co>
2024-08-27 17:05:35 -0500

    Primer ajuste 7071 registros nuevos

--- Cargue.py
+++ Cargue.py
@@ -1,4 +1,27 @@
-import pandas as np
+import pandas as pd
+
+file_path = 'DETALLADO_AGOSTO27_2024.xlsx'
+Nuevo = pd.read_excel(file_path, engine= 'openpyxl')
+
+file_path2 = 'DETALLADO_FEBRERO29_2024.xlsx'
+Original = pd.read_excel(file_path2, engine= 'openpyxl')
+
+# Asegurarse de que la columna de ID tenga el mismo nombre en ambos DataFrames
+id_columna = 'PER_ID'
+
+# Buscar registros en df_comparar que no estén en df_base
+nuevos_registros = Nuevo[~Nuevo[id_columna].isin(Original[id_columna])]
+
+# Insertar nuevos registros en Original
+df_actualizado = pd.concat([Original, nuevos_registros], ignore_index=True)
+
+# Guardar el DataFrame actualizado en un nuevo archivo Excel o sobreescribir el existente
+df_actualizado.to_excel('archivo_base_actualizado.xlsx', index=False)
+
+print(f'Se han insertado {len(nuevos_registros)} registros nuevos en el archivo base.')
+
+#print(Nuevo.head())
+
 
 
 
